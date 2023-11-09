57807620440b2412f810e986a0874c661b511f66
Ricardo CORIN <ricardocorin@ricardos-mbp.home>
2023-11-09 12:03:43 -0300

    initial project

---
+++ Pokemon/Pokemon/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pokemon
+//
+//  Created by Ricardo CORIN on 11/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pokemon/Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Ricardo CORIN on 11/9/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
